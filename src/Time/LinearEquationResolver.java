fcbec96341f0d9d8a6e947a7ce584c460f75dc17
kkingkangg <phungnghia152@gmail.com>
2020-09-09 21:18:54 +0700

    phương trình bậc nhất

---
+++ src/Time/LinearEquationResolver.java
@@ -0,0 +1,31 @@
+package Time;
+import java.util.Scanner;
+
+
+public class LinearEquationResolver {
+    public static void main(String[] args) {
+       System.out.println("Linear Equation Resolver");
+       System.out.println("Given a equation as 'a * x + b = 0', please enter constants:");
+       Scanner scanner = new Scanner(System.in);
+
+       System.out.println("a: ");
+       double a = scanner.nextDouble();
+
+       System.out.println("b: ");
+       double b = scanner.nextDouble();
+
+        System.out.println("c: ");
+        double c = scanner.nextDouble();
+
+        if (a != 0) {
+            double solution = (c-b)/a;
+            System.out.println ("The solution is: %f!\n" + solution);
+        } else {
+            if (b==0) {
+                System.out.println("The solution is all x!");
+            } else {
+                System.out.println("No solution!");
+            }
+        }
+    }
+}
